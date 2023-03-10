a40439df2ffd34c2799c3d0102ff131e924cad50
LocaZika <60792232+LocaZika@users.noreply.github.com>
2023-03-10 23:37:03 +0700

    add loader

---
+++ src/services/Loader.jsx
@@ -0,0 +1,47 @@
+import '../assets/scss/_loader.scss';
+import MusicPlayer from '../components/MusicPlayer';
+import { get } from '../api/RESTfulApi';
+import withContext from '../components/stateGlobal/withContext';
+import { Component } from 'react';
+class Loader extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false
+    }
+  }
+  getSongs = () => {
+    const { dispatch } = this.props.context;
+    get().then(({ data: songs }) => {
+        dispatch.setSongs(songs);
+        dispatch.setCurrentSong(songs[0]);
+      }
+    )
+    window.addEventListener('load', () => {
+      setTimeout(() => this.setState({ isLoaded: true }), 1000);
+    });
+  };
+  componentDidMount(){
+    this.getSongs();
+
+  }
+  render(){
+    if(this.state.isLoaded === false) {
+      return (
+        <div className="preloader">
+          <div className="loading">
+            <span style={{animationDelay: '0s'}}></span>
+            <span style={{animationDelay: '0.2s'}}></span>
+            <span style={{animationDelay: '0.4s'}}></span>
+            <span style={{animationDelay: '0.6s'}}></span>
+          </div>
+        </div>
+      )
+    }else{
+      return (
+        <MusicPlayer />
+      )
+    }
+  }
+}
+export default withContext(Loader);
